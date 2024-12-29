9628160a6f1d6eea7080058f5c0d3f50f9a5dcd6
Marcos Legido <lainmlh@gmail.com>
2024-12-29 21:34:32 +0100

    DB annotations

---
+++ src/jstart/annotations/RecordTable.java
@@ -0,0 +1,12 @@
+package jstart.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface RecordTable {
+    public String table() default "";
+}
