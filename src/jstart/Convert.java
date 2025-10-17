2e1f6dd0ebb8aae086dd70657b809df34112b7ad
Marcos Legido <lainmlh@gmail.com>
2025-10-17 23:29:48 +0200

    isNull function and documentation

--- src/jstart/Convert.java
+++ src/jstart/Convert.java
@@ -1,5 +1,8 @@
 package jstart;
 
+/**
+ * Allow convert an object to another data types
+ */
 public final class Convert {
 
     private Object number;
@@ -8,16 +11,40 @@ public final class Convert {
         this.number = number;
     }
 
+    /**
+     * get if data to convert is null
+     * 
+     * @return true if data is null
+     */
+    public boolean isNull() {
+        return number == null;
+    }
+
+    /**
+     * Sets the data to convert to another type
+     * 
+     * @param object
+     * @return
+     */
     public static Convert from(Object object) {
         return new Convert(object);
     }
 
+    /**
+     * Gets the integer part of a number string
+     * 
+     * @param number number string
+     * @return integer part of number string
+     */
     private static String getIntegerValue(String number) {
-        return number.matches("[0-9]+[\\.|,].+") ?
-            number.split("[\\.|,]")[0] :
-            number;
+        return number.matches("[0-9]+[\\.|,].+") ? number.split("[\\.|,]")[0] : number;
     }
 
+    /**
+     * Converts the data to Byte
+     * 
+     * @return Byte value
+     */
     public Byte toByte() {
         try {
             return Byte.valueOf(getIntegerValue(number.toString()));
@@ -26,6 +53,11 @@ public final class Convert {
         }
     }
 
+    /**
+     * Converts the data to Short
+     * 
+     * @return Short value
+     */
     public Short toShort() {
         try {
             return Short.valueOf(getIntegerValue(number.toString()));
@@ -34,6 +66,11 @@ public final class Convert {
         }
     }
 
+    /**
+     * Converts the data to Integer
+     * 
+     * @return Integer value
+     */
     public Integer toInteger() {
         try {
             return Integer.valueOf(getIntegerValue(number.toString()));
@@ -42,6 +79,11 @@ public final class Convert {
         }
     }
 
+    /**
+     * Converts the data to Long
+     * 
+     * @return Long value
+     */
     public Long toLong() {
         try {
             return Long.valueOf(getIntegerValue(number.toString()));
@@ -50,6 +92,11 @@ public final class Convert {
         }
     }
 
+    /**
+     * Converts the data to Float
+     * 
+     * @return Float value
+     */
     public Float toFloat() {
         try {
             return Float.valueOf(number.toString());
@@ -58,6 +105,11 @@ public final class Convert {
         }
     }
 
+    /**
+     * Converts the data to Double
+     * 
+     * @return Double value
+     */
     public Double toDouble() {
         try {
             return Double.valueOf(number.toString());
@@ -66,6 +118,11 @@ public final class Convert {
         }
     }
 
+    /**
+     * Converts the data to Boolean
+     * 
+     * @return Boolean value
+     */
     public Boolean toBoolean() {
         try {
             return Boolean.valueOf(number.toString());
@@ -74,9 +131,11 @@ public final class Convert {
         }
     }
 
+    /**
+     * Gets the string representation of the data, or null if data is null
+     */
     @Override
     public String toString() {
         return number != null ? number.toString() : null;
     }
 }
-
